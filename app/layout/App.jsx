41216070086c22cef5485ad93b566ed1072be6f9
ronniekiyegga <ronniekiyegga@hotmail.com>
2021-11-01 23:43:45 +0000

    initial commit

---
+++ app/layout/App.jsx
@@ -0,0 +1,11 @@
+import Reacr from 'react'
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Spex</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './app/layout/style.css';
+import App from '../src/app/layout/App.jsx';
+import reportWebVitals from './reportWebVitals';
+
+const rootEl = document.getElementById('root')
+
+function render() {
+  ReactDOM.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>,
+    rootEl
+  );
+}
+
+if (module.hot) {
+  module.hot.accept('../src/app/layout/App.jsx', function() {
+    setTimeout(render)
+  })
+}
+
+render();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
